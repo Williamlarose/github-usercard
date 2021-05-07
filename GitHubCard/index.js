0e4c29f1794df144ab32ee8a563e4c4d0304b520
Williamlarose <38011333+Williamlarose@users.noreply.github.com>
2021-05-06 23:28:51 -0400

    dine

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,13 +1,16 @@
+import axios from 'axios'
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
 
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
     data in order to use it to build your component function
+    
 
     Skip to STEP 3.
 */
@@ -28,7 +31,11 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetondan',
+  'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell'];
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -58,3 +65,66 @@ const followersArray = [];
     luishrd
     bigknell
 */
+const cards = document.querySelector('div.cards')
+function makeaProfile(obj) {
+  const card = document.createElement('div')
+  const img = document.createElement('img')
+  const info = document.createElement('div')
+  const name = document.createElement('h3')
+  const para = document.createElement('p')
+  const para1 = document.createElement('p')
+  const profile = document.createElement('p')
+  const link = document.createElement('a')
+  const follow = document.createElement('p')
+  const follow2 = document.createElement('p')
+  const bio = document.createElement('p')
+
+
+  card.appendChild(info)
+  info.appendChild(name)
+  info.appendChild(para)
+  info.appendChild(para1)
+  info.appendChild(profile)
+  info.appendChild(link)
+  info.appendChild(follow)
+  info.appendChild(follow2)
+  info.appendChild(bio)
+  profile.appendChild(img)
+
+  card.classList.add('card')
+  info.classList.add('card-info')
+  name.classList.add('name')
+  para.classList.add('username')
+
+  link.href = obj.html_url
+  img.src = obj.avatar_url
+  para.textContent = obj.login
+  name.textContent = obj.name
+  para1.textContent = obj.location
+  link.textContent = obj.html_url
+  profile.textContent = 'Profile:'
+
+  follow.textContent = obj.followers
+  follow2.textContent = obj.following
+  bio.textContent = 'Bio'
+  card.appendChild(info)
+  info.appendChild(name)
+  info.appendChild(para)
+  info.appendChild(para1)
+  info.appendChild(profile)
+  info.appendChild(img)
+  info.appendChild(follow)
+  info.appendChild(follow2)
+  info.appendChild(bio)
+  profile.appendChild(link)
+  return card
+}
+followersArray.forEach(function (obj) {
+
+  axios.get(`https://api.github.com/users/${obj}`)
+    .then(function (yo) {
+
+      const pro = makeaProfile(yo.data)
+      cards.appendChild(pro)
+    })
+})
